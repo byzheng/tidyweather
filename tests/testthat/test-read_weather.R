d347da2fcf717d186de5c5da9de0e6601541de22
Bangyou Zheng <zheng.bangyou@gmail.com>
2025-10-28 19:44:04 +1000

    Add more tests

--- tests/testthat/test-read_weather.R
+++ tests/testthat/test-read_weather.R
@@ -11,4 +11,8 @@ test_that("Weather records for apsim format", {
 
     records <- read_weather(file)
     expect_true(all(c("date", "year", "day", "maxt", "mint", "radn", "rain") %in% names(records)))
+    expect_equal(nrow(records), 587)
+    expect_equal(records$year[1], 2023)
+    expect_equal(records$date[1], as.Date("2023-12-01"))
+    expect_equal(records$mint[1], 14.4)
 })
