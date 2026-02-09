5b7514eb7c2fcec6a0d0cedf9de022939b30d7f0
Bangyou Zheng <zheng.bangyou@gmail.com>
2026-02-09 22:07:05 +1000

    use optree

--- R/utilities.R
+++ R/utilities.R
@@ -104,8 +104,7 @@ day_length <- function( doy, lat, angle = -6 ) {
 #' @param split split
 #' @return The interpolated values
 #' @export
-interpolation_function <- function( x, y, values, split = '\\s+' )
-{
+interpolation_function <- function( x, y, values, split = '\\s+' ) {
     if (is.character(x) & length(x) == 1)
     {
         x <- as.numeric(strsplit(x, split)[[1]])
@@ -131,3 +130,36 @@ interpolation_function <- function( x, y, values, split = '\\s+' )
 }
 
 
+.check_full_year <- function(df) {
+    if (!tibble::has_name(df, "day")) {
+        stop("Input data should include a column 'day' for day of year.")
+    }
+    if (nrow(df) != 365 && nrow(df) != 366) {
+        stop(
+            "Data does not contain a full year. ",
+            "Set require_full_year = FALSE to proceed anyway."
+        )
+    }
+    if (min(df$day) != 1 || max(df$day) != nrow(df)) {
+        stop(
+            "Data does not contain a full year. ",
+            "Set require_full_year = FALSE to proceed anyway."
+        )
+    }
+}
+
+
+# Detect hemisphere
+.detect_hemisphere <- function(df, hemisphere) {
+    if (tibble::has_name(df, "latitude")) {
+        if (length(unique(df$latitude)) != 1) {
+            stop("Latitude values are not consistent within a group.")
+        }
+        if (df$latitude[1] < 0) {
+            return("south")
+        } else {
+            return("north")
+        }
+    }
+    match.arg(hemisphere, c("south", "north"))
+}
